750583bb6ce492bec772ab26009bdc96f80b58c7
Bruno Murta <brunoh.murta@gmail.com>
2020-10-14 13:01:58 -0300

    Git inicial do projeto - Sincronizando ambientes

---
+++ src/main/java/com/murta/senai/tcc/halcApi/HalcApiApplication.java
@@ -0,0 +1,13 @@
+package com.murta.senai.tcc.halcApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalcApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/murta/senai/tcc/halcApi/HalcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murta.senai.tcc.halcApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
